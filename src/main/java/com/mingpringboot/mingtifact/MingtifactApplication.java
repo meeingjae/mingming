9e127097a59e283a62bd2106c88e2dedad917f50
mingming <dlsqo2005@naver.com>
2019-09-19 15:47:05 +0900

    version 1.0.0

---
+++ src/main/java/com/mingpringboot/mingtifact/MingtifactApplication.java
@@ -0,0 +1,16 @@
+package com.mingpringboot.mingtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingtifactApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MingtifactApplication.class, args);
+
+
+    }
+
+}
